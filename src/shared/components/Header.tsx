08b9ff49103ffb61956825795ff2aae2f87440dc
Haleem Rafat <haleem.rafat@gmail.com>
2025-07-31 17:04:47 +0300

    Enhance Header component with responsive design adjustments, including mobile-specific elements and improved layout. Add new icons and update promotional text display based on screen size.

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -2,65 +2,90 @@
 
 import { useState } from 'react';
 import Image from 'next/image';
-import { BiSearch } from 'react-icons/bi';
+import { BiChevronDown, BiSearch } from 'react-icons/bi';
+import { HiOutlineMenuAlt3 } from 'react-icons/hi';
 import { Button, DropdownMenu, HeaderIllustrations, Input, MainTadarabLogo } from '@/shared';
 import starIcon from '@/assets/icons/star.svg';
 import shopCarIcon from '@/assets/icons/shop-car.svg';
+import useMediaQuery from '@/hooks/useMediaQuery';
 
 export default function Header() {
+  const isMobile = useMediaQuery('(max-width: 768px)');
+
   const [searchName, setSearchName] = useState('');
   console.log(searchName);
 
   return (
     <div className="fixed top-0 right-0 left-0 z-50">
-      <div className="absolute left-1/2 -translate-x-1/2">
+      <div className="absolute left-1/2 hidden -translate-x-1/2 lg:block">
         <HeaderIllustrations />
       </div>
-      <div className="bg-header-background backdrop-blur-header-background-blur">
-        <div className="mx-auto flex max-w-[1440px] items-center justify-center gap-5 py-3 text-sm font-bold text-white">
+      <div className="bg-header-background backdrop-blur-header-background-blur px-5">
+        <div className="mx-auto flex max-w-[1440px] items-center justify-between gap-5 py-3 text-sm font-bold text-white lg:justify-center">
           <Button variant="underline" className="relative z-50 text-sm font-extrabold">
             اشترك الآن
           </Button>
-          <p>خصم 80% بمناسبة شهر رمضان الكريم لفترة محدودة</p>
+          <p className="relative z-50 text-xs font-bold lg:text-base">
+            {isMobile
+              ? 'خصم 80% بمناسبة شهر رمضان الكريم'
+              : 'خصم 80% بمناسبة شهر رمضان الكريم لفترة محدودة'}
+          </p>
         </div>
       </div>
-      <div className="bg-background relative z-50">
-        <div className="mx-auto flex w-full max-w-[1440px] items-center justify-between gap-5 py-3">
-          <Button variant="text" className="py-3 text-sm font-bold">
-            <Image src={shopCarIcon} alt="shop-car" />
-          </Button>
-          <Button variant="secondary" className="px-5 py-3 text-sm font-bold">
+      <div className="bg-background relative z-50 px-5">
+        <div className="mx-auto flex w-full max-w-[1440px] items-center justify-between gap-5 py-2">
+          <div className="flex items-center gap-3">
+            <Button variant="text" className="py-3 text-sm font-bold">
+              <Image src={shopCarIcon} alt="shop-car" />
+              <Button variant="text" className="block py-3 text-sm font-bold lg:hidden">
+                <BiSearch size={24} />
+              </Button>
+            </Button>
+          </div>
+
+          <Button variant="secondary" className="hidden px-5 py-3 text-sm font-bold lg:block">
             تسجيل الدخول
           </Button>
           <Button
-            rightIcon={<Image src={starIcon} alt="star" />}
             variant="tinted"
-            className="px-5 py-3 text-sm font-bold">
+            rightIcon={<Image src={starIcon} alt="star" />}
+            className="hidden px-5 py-3 text-sm font-bold lg:flex">
             تدرب بلا حدود
           </Button>
-          <Button variant="text" className="py-3 text-sm font-bold">
+          <Button variant="text" className="hidden py-3 text-sm font-bold lg:block">
             تدرب للأعمال
           </Button>
-          <Button variant="text" className="py-3 text-sm font-bold">
+          <Button variant="text" className="hidden py-3 text-sm font-bold lg:block">
             انضم كمدرب
           </Button>
 
-          <Input
-            dir="rtl"
-            value={searchName}
-            className="placeholder:text-input-placeholder w-full flex-1"
-            onChange={(e: any) => setSearchName(e.target.value)}
-            rightIcon={<BiSearch size={16} />}
-            placeholder={`ماذا تريد ان تتعلم اليوم؟`}
-          />
+          {!isMobile && (
+            <Input
+              dir="rtl"
+              value={searchName}
+              className="placeholder:text-input-placeholder w-full flex-1"
+              onChange={(e: any) => setSearchName(e.target.value)}
+              rightIcon={<BiSearch size={16} />}
+              placeholder={`ماذا تريد ان تتعلم اليوم؟`}
+            />
+          )}
+
           <DropdownMenu
+            className="hidden lg:block"
             trigger={
-              <Button variant="text" className="py-3 text-sm font-bold">
-                استكشف الأقسام{' '}
+              <Button
+                leftIcon={<BiChevronDown size={20} className="text-input-placeholder" />}
+                variant="text"
+                className="py-3 text-sm font-bold">
+                استكشف الأقسام
               </Button>
             }
           />
           <MainTadarabLogo />
+
+          <Button variant="text" className="block py-3 text-sm font-bold lg:hidden">
+            <HiOutlineMenuAlt3 size={24} />
+          </Button>
         </div>
       </div>
     </div>
