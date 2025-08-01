a9633afa8679f26a19ba1f87a21c166e348a2c6c
Haleem Rafat <haleem.rafat@gmail.com>
2025-08-01 20:38:26 +0300

    Adjust border radius in MainCourseCard component for improved layout consistency across screen sizes.

--- src/shared/ui/MainCourseCard.tsx
+++ src/shared/ui/MainCourseCard.tsx
@@ -53,7 +53,7 @@ export default function MainCourseCard({
           handleCardClick();
         }
       }}>
-      <div className={`relative h-[340px] overflow-hidden rounded-none lg:h-[180px] lg:rounded-xl`}>
+      <div className={`relative h-[340px] overflow-hidden rounded-xl lg:h-[180px] lg:rounded-none`}>
         {!imageError ? (
           <>
             <Image
